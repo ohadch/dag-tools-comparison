562936e877523f9ea7e9028f6fec6df8e79f9f43
ohadchaet <ohad.chaet@lynx.md>
2022-06-11 10:55:19 +0300

    Implements get_data task

--- prefect/flows/airflow_tutorial_flow.py
+++ prefect/flows/airflow_tutorial_flow.py
@@ -1,4 +1,7 @@
+import os
+
 import psycopg2
+import requests
 from prefect import task
 
 
@@ -39,30 +42,31 @@ def create_employees_temp_table():
             conn.commit()
 
 
+@task
+def get_data():
+    # NOTE: configure this as appropriate for your airflow environment
+    data_path = "/opt/airflow/dags/files/employees.csv"
+    os.makedirs(os.path.dirname(data_path), exist_ok=True)
 
-#     @task
-#     def get_data():
-#         # NOTE: configure this as appropriate for your airflow environment
-#         data_path = "/opt/airflow/dags/files/employees.csv"
-#         os.makedirs(os.path.dirname(data_path), exist_ok=True)
-#
-#         url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/pipeline_example.csv"
-#
-#         response = requests.request("GET", url)
-#
-#         with open(data_path, "w") as file:
-#             file.write(response.text)
-#
-#         postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
-#         conn = postgres_hook.get_conn()
-#         cur = conn.cursor()
-#         with open(data_path, "r") as file:
-#             cur.copy_expert(
-#                 "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
-#                 file,
-#             )
-#         conn.commit()
-#
+    url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/pipeline_example.csv"
+
+    response = requests.request("GET", url)
+
+    with open(data_path, "w") as file:
+        file.write(response.text)
+
+    with psycopg2.connect(
+        host="localhost",
+        port="5436"
+    ) as conn:
+        with conn.cursor() as cur:
+            with open(data_path, "r") as file:
+                cur.copy_expert(
+                    "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                    file,
+                )
+            conn.commit()
+
 #     @task
 #     def merge_data():
 #         query = """
